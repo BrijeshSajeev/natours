1ad2a30a3d6e9c9016d7f10eb843adcb601af656
BrijeshSajeev <brijesh00300@gmail.com>
2023-08-16 19:17:56 +0530

    new:initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// File system
+const fs = require('fs');
+// Express
+const express = require('express');
+const exp = require('constants');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((ele) => ele.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const addTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+// // Get request
+// app.get('/api/v1/tours', getTours);
+// Post request
+// app.post('/api/v1/tours', addTour);
+
+app.route('/api/v1/tours').get(getTour).post(addTour);
+
+// Get element by Id
+app.get('/api/v1/tours/:id', getTour);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('System is running');
+});
