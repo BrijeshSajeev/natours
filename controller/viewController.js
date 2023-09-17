95c5a5f56561b255c6c5d22ceda358c48305b503
BrijeshSajeev <brijesh00300@gmail.com>
2023-09-17 22:44:34 +0530

    new: Overview Page

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../model/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //   1 get all tours
+  const tours = await Tour.find();
+  // build the template
+
+  // send the data to the template form 1
+
   res.status(200).render('overview', {
     title: ' hello world',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
