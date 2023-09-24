f12a0cbd53b3aa062d467be1cbb5c27f12527b79
BrijeshSajeev <brijesh00300@gmail.com>
2023-09-24 19:58:19 +0530

    new : welcome email

--- controller/authController.js
+++ controller/authController.js
@@ -7,6 +7,9 @@ const AppError = require('../utils/appError');
 
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
+const Emial = require('../utils/email');
+const Email = require('../utils/email');
+
 // const sendEmail = require('../utils/email');
 
 const signToken = (id) =>
@@ -48,7 +51,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+  const url = `${req.protocol}://${req.get('host')}/me`;
 
+  await new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 200, res);
 });
 
@@ -180,6 +185,8 @@ exports.forgetPassword = catchAsync(async (req, res, next) => {
     //   subject: 'Your password reset token (10 mins)',
     //   message,
     // });
+
+    await new Email(user, resetUrl).sendPasswordReset();
 
     res.status(200).json({
       email: user.email,
--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,86 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Brijesh Sajeev <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      // return nodemailer.createTransport({
+      //   service: 'SendGrid',
+      //   auth: {
+      //     user: process.env.SENDGRID_USERNAME,
+      //     pass: process.env.SENDGRID_PASSWORD,
+      //   },
+      // });
+    }
+
+    // host: process.env.EMAIL_HOST,
+    // port: process.env.EMAIL_PORT,
+    // auth: {
+    //   user: process.env.EMAIL_USERNAME,
+    //   pass: process.env.EMAIL_PASSWORD,
+    // },
+    return nodemailer.createTransport({
+      host: 'sandbox.smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: 'a96c3d66fc5845',
+        pass: '74a080c54c4588',
+      },
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text: htmlToText.convert(html),
+      html,
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)',
+    );
+  }
+};
+
+/*
 const sendEmail = async (options) => {
   // 1 Create Transporter
-  const transporter = nodemailer.createTransport({
-    host: 'sandbox.smtp.mailtrap.io',
-    port: 465,
-    auth: {
-      user: 'a96c3d66fc5845',
-      pass: '74a080c54c4588',
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: 'sandbox.smtp.mailtrap.io',
+  //   port: 465,
+  //   auth: {
+  //     user: 'a96c3d66fc5845',
+  //     pass: '74a080c54c4588',
+  //   },
+  // });
   //   2 Define the email option
   const mailOptions = {
     from: 'Brijesh sajeev <hello@brijesh.io>',
@@ -25,4 +96,4 @@ const sendEmail = async (options) => {
   console.log(info);
 };
 
-module.exports = sendEmail;
+*/
