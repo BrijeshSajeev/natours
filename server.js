6c7110cd41861b445d15a600552e0cde9f3b2758
BrijeshSajeev <brijesh00300@gmail.com>
2023-09-29 19:09:14 +0530

    updated server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
     console.log('Connection Successfull');
   });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log('System is running');
 });
